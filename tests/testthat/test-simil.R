db9f60fca1358c6fff96fb95d804f1b1175118c7
Robrecht Cannoodt <rcannood@gmail.com>
2019-04-15 12:20:43 +0200

    add test for detecting problem outlined in koheiw/proxyC#3

--- tests/testthat/test-simil.R
+++ tests/testthat/test-simil.R
@@ -3,27 +3,43 @@ context("test simil")
 test_mt <- Matrix::rsparsematrix(100, 100, 0.5)
 
 test_simil <- function(x, method, margin, ignore_upper = FALSE, ignore_diag = TRUE, ...) {
-
-    if (margin == 1) {
-        by_rows <- TRUE
-        y <- x[sample(nrow(x), 10),]
-    } else {
-        by_rows <- FALSE
-        y <- x[,sample(ncol(x), 10)]
-    }
-
+    # test with only x
     s1 <- as.matrix(simil(x, method = method, margin = margin, ...))
     s2 <- proxy::as.matrix(proxy::simil(as.matrix(x),
-                                method = method, by_rows = by_rows, diag = TRUE, ...))
+                                method = method, by_rows = margin == 1, diag = TRUE, ...))
     if (ignore_diag)
         diag(s1) <- diag(s2) <- 0
     if (ignore_upper)
         s1[upper.tri(s1, TRUE)] <- s2[upper.tri(s2, TRUE)] <- 0
     expect_equal(as.numeric(s1), as.numeric(s2), tolerance = 0.001)
 
+    # test with x and y, different size
+    if (margin == 1) {
+        y <- x[sample(nrow(x), 10),]
+    } else {
+        y <- x[,sample(ncol(x), 10)]
+    }
+
     s3 <- as.matrix(simil(x, y, method = method, margin = margin, ...))
     s4 <- as.matrix(proxy::simil(as.matrix(x), as.matrix(y),
-                                method = method, by_rows = by_rows, diag = TRUE, ...))
+                                 method = method, by_rows = margin == 1, diag = TRUE, ...))
+    if (ignore_diag)
+        diag(s3) <- diag(s4) <- 0
+    if (ignore_upper)
+        s3[upper.tri(s3, TRUE)] <- s4[upper.tri(s4, TRUE)] <- 0
+    expect_equal(as.numeric(s3), as.numeric(s4), tolerance = 0.001)
+
+
+    # test with x and y, same size
+    if (margin == 1) {
+        y <- x[sample(nrow(x)),]
+    } else {
+        y <- x[,sample(ncol(x))]
+    }
+
+    s3 <- as.matrix(simil(x, y, method = method, margin = margin, ...))
+    s4 <- as.matrix(proxy::simil(as.matrix(x), as.matrix(y),
+                                 method = method, by_rows = margin == 1, diag = TRUE, ...))
     if (ignore_diag)
         diag(s3) <- diag(s4) <- 0
     if (ignore_upper)
