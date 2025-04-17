c131a134b4c7c3ab1020ccb264aee71a25ad1be1
Kohei Watanabe <watanabe.kohei@gmail.com>
2025-04-17 11:51:09 +0900

    Change the order of arguments

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -12,15 +12,15 @@ Rcpp::Rostream<false>& Rcpp::Rcerr = Rcpp::Rcpp_cerr_get();
 #endif
 
 // cpp_linear
-S4 cpp_linear(arma::sp_mat& mt1, arma::sp_mat& mt2, arma::sp_mat& mask, const int method, unsigned int rank, const double limit, bool symm, const bool drop0, const bool use_nan, const bool use_mask, const bool sparse, const int digits, const int thread);
-RcppExport SEXP _proxyC_cpp_linear(SEXP mt1SEXP, SEXP mt2SEXP, SEXP maskSEXP, SEXP methodSEXP, SEXP rankSEXP, SEXP limitSEXP, SEXP symmSEXP, SEXP drop0SEXP, SEXP use_nanSEXP, SEXP use_maskSEXP, SEXP sparseSEXP, SEXP digitsSEXP, SEXP threadSEXP) {
+S4 cpp_linear(arma::sp_mat& mt1, arma::sp_mat& mt2, const int method, arma::sp_mat& mask, unsigned int rank, const double limit, bool symm, const bool drop0, const bool use_nan, const bool use_mask, const bool sparse, const int digits, const int thread);
+RcppExport SEXP _proxyC_cpp_linear(SEXP mt1SEXP, SEXP mt2SEXP, SEXP methodSEXP, SEXP maskSEXP, SEXP rankSEXP, SEXP limitSEXP, SEXP symmSEXP, SEXP drop0SEXP, SEXP use_nanSEXP, SEXP use_maskSEXP, SEXP sparseSEXP, SEXP digitsSEXP, SEXP threadSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
     Rcpp::traits::input_parameter< arma::sp_mat& >::type mt1(mt1SEXP);
     Rcpp::traits::input_parameter< arma::sp_mat& >::type mt2(mt2SEXP);
-    Rcpp::traits::input_parameter< arma::sp_mat& >::type mask(maskSEXP);
     Rcpp::traits::input_parameter< const int >::type method(methodSEXP);
+    Rcpp::traits::input_parameter< arma::sp_mat& >::type mask(maskSEXP);
     Rcpp::traits::input_parameter< unsigned int >::type rank(rankSEXP);
     Rcpp::traits::input_parameter< const double >::type limit(limitSEXP);
     Rcpp::traits::input_parameter< bool >::type symm(symmSEXP);
@@ -30,7 +30,7 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< const bool >::type sparse(sparseSEXP);
     Rcpp::traits::input_parameter< const int >::type digits(digitsSEXP);
     Rcpp::traits::input_parameter< const int >::type thread(threadSEXP);
-    rcpp_result_gen = Rcpp::wrap(cpp_linear(mt1, mt2, mask, method, rank, limit, symm, drop0, use_nan, use_mask, sparse, digits, thread));
+    rcpp_result_gen = Rcpp::wrap(cpp_linear(mt1, mt2, method, mask, rank, limit, symm, drop0, use_nan, use_mask, sparse, digits, thread));
     return rcpp_result_gen;
 END_RCPP
 }
@@ -69,15 +69,15 @@ BEGIN_RCPP
 END_RCPP
 }
 // cpp_pair
-S4 cpp_pair(arma::sp_mat& mt1, arma::sp_mat& mt2, arma::sp_mat& mask, const int method, unsigned int rank, const double limit, const double weight, const double smooth, bool symm, const bool diag, const bool drop0, const bool use_nan, const bool use_mask, const bool sparse, const int digits, const int thread);
-RcppExport SEXP _proxyC_cpp_pair(SEXP mt1SEXP, SEXP mt2SEXP, SEXP maskSEXP, SEXP methodSEXP, SEXP rankSEXP, SEXP limitSEXP, SEXP weightSEXP, SEXP smoothSEXP, SEXP symmSEXP, SEXP diagSEXP, SEXP drop0SEXP, SEXP use_nanSEXP, SEXP use_maskSEXP, SEXP sparseSEXP, SEXP digitsSEXP, SEXP threadSEXP) {
+S4 cpp_pair(arma::sp_mat& mt1, arma::sp_mat& mt2, const int method, arma::sp_mat& mask, unsigned int rank, const double limit, const double weight, const double smooth, bool symm, const bool diag, const bool drop0, const bool use_nan, const bool use_mask, const bool sparse, const int digits, const int thread);
+RcppExport SEXP _proxyC_cpp_pair(SEXP mt1SEXP, SEXP mt2SEXP, SEXP methodSEXP, SEXP maskSEXP, SEXP rankSEXP, SEXP limitSEXP, SEXP weightSEXP, SEXP smoothSEXP, SEXP symmSEXP, SEXP diagSEXP, SEXP drop0SEXP, SEXP use_nanSEXP, SEXP use_maskSEXP, SEXP sparseSEXP, SEXP digitsSEXP, SEXP threadSEXP) {
 BEGIN_RCPP
     Rcpp::RObject rcpp_result_gen;
     Rcpp::RNGScope rcpp_rngScope_gen;
     Rcpp::traits::input_parameter< arma::sp_mat& >::type mt1(mt1SEXP);
     Rcpp::traits::input_parameter< arma::sp_mat& >::type mt2(mt2SEXP);
-    Rcpp::traits::input_parameter< arma::sp_mat& >::type mask(maskSEXP);
     Rcpp::traits::input_parameter< const int >::type method(methodSEXP);
+    Rcpp::traits::input_parameter< arma::sp_mat& >::type mask(maskSEXP);
     Rcpp::traits::input_parameter< unsigned int >::type rank(rankSEXP);
     Rcpp::traits::input_parameter< const double >::type limit(limitSEXP);
     Rcpp::traits::input_parameter< const double >::type weight(weightSEXP);
@@ -90,7 +90,7 @@ BEGIN_RCPP
     Rcpp::traits::input_parameter< const bool >::type sparse(sparseSEXP);
     Rcpp::traits::input_parameter< const int >::type digits(digitsSEXP);
     Rcpp::traits::input_parameter< const int >::type thread(threadSEXP);
-    rcpp_result_gen = Rcpp::wrap(cpp_pair(mt1, mt2, mask, method, rank, limit, weight, smooth, symm, diag, drop0, use_nan, use_mask, sparse, digits, thread));
+    rcpp_result_gen = Rcpp::wrap(cpp_pair(mt1, mt2, method, mask, rank, limit, weight, smooth, symm, diag, drop0, use_nan, use_mask, sparse, digits, thread));
     return rcpp_result_gen;
 END_RCPP
 }
