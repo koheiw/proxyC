020e87fadd41c400975aefba3c8139f68c4d9d5b
Kohei Watanabe <watanabe.kohei@gmail.com>
2025-04-17 10:26:39 +0900

    Enable masking only in simil()

--- R/mask.R
+++ R/mask.R
@@ -1,13 +1,18 @@
 #' Create a pattern matrix for masking
 #'
-#' Create a pattern matrix for `simil()` or `dist()` to enable masking.
+#' Create a pattern matrix for `simil()` to enable masked similarity computation.
+#' If the matrix is passed to `simil()`, it computes similarity scores only for cells with `TRUE`.
 #' @param x,y a numeric or character vector to match against each other.
 #' @return a sparse logical matrix with `TRUE` for matched pairs.
 #' @export
 #' @examples
-#' mask(c("a", "b", "c", "d", "e"),
-#'      c("a", "a", "d", "d", "e", "e"))
+#' mt1 <- Matrix::rsparsematrix(100, 6, 1.0)
+#' colnames(mt1) <- c("a", "a", "d", "d", "e", "e")
+#' mt2 <- Matrix::rsparsematrix(100, 5, 1.0)
+#' colnames(mt2) <- c("a", "b", "c", "d", "e")
 #'
+#' (msk <- mask(colnames(mt1), colnames(mt2)))
+#' simil(mt1, mt2, margin = 2, mask = msk, drop0 = TRUE)
 mask <- function(x, y) {
 
     if (!identical(class(x), class(y)))
