18062ae8d2be6c625c6e71b13f15b84700392dc3
Kohei Watanabe <watanabe.kohei@gmail.com>
2025-04-21 10:37:22 +0900

    Add tcrossprod()

--- R/crossprod.R
+++ R/crossprod.R
@@ -1,4 +1,7 @@
-#' Compute the cross product of sparse matrices
+#' Compute the cross product of large sparse matrices
+#'
+#' Compute the (transposed) cross product of large sparse matrices using the same
+#' infrastructure as [simil()] and [dist()].
 #' @inheritParams simil
 #' @param min_prod the minimum product to be recorded.
 #' @export
@@ -10,3 +13,12 @@ crossprod <- function(x, y = NULL, min_prod = NULL, digits = 14) {
           digits = digits)
 }
 
+#' @rdname crossprod
+#' @export
+tcrossprod <- function(x, y = NULL, min_prod = NULL, digits = 14) {
+
+    if (is.null(min_prod))
+        min_prod <- -Inf
+    proxy(x, y, margin = 1, method = "product", min_proxy = min_prod,
+          digits = digits)
+}
