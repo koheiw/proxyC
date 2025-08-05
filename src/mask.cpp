0e49ccb5f8b25bf8cebe5269e9df869aec01a8bd
Kohei Watanabe <watanabe.kohei@gmail.com>
2025-08-05 09:36:10 +0900

    Add cpp_mask_update()

--- src/mask.cpp
+++ src/mask.cpp
@@ -38,6 +38,57 @@ S4 cpp_mask(IntegerVector v1_,
     return to_matrix(simil_tri, I, J, false, true);
 }
 
+// [[Rcpp::export]]
+S4 cpp_mask_update(IntegerVector v1_,
+                   IntegerVector v2_,
+                   arma::sp_mat& mask,
+                   int operation = 1,
+                   const int thread = -1) {
+
+    std::vector<int> v1 = Rcpp::as<std::vector<int>>(v1_);
+    std::vector<int> v2 = Rcpp::as<std::vector<int>>(v2_);
+
+    std::size_t I = v1.size();
+    std::size_t J = v2.size();
+    Triplets simil_tri;
+
+#if PROXYC_USE_TBB
+    tbb::task_arena arena(thread);
+    arena.execute([&]{
+        tbb::parallel_for(tbb::blocked_range<int>(0, I), [&](tbb::blocked_range<int> r) {
+            for (int i = r.begin(); i < r.end(); i++) {
+                for (std::size_t j = 0; j < J; j++) {
+                    double v = 0.0;
+                    if (operation == 1) {
+                        v = (bool)mask(i, j) && (v1[i] == v2[j]);
+                    } else if (operation == 2) {
+                        v = (bool)mask(i, j) || (v1[i] == v2[j]);
+                    } else {
+                        v = (bool)mask(i, j) != (v1[i] == v2[j]);
+                    }
+                    simil_tri.push_back(std::make_tuple(i, j, v));
+                }
+            }
+        });
+    });
+# else
+    for (std::size_t i = 0; i < I; i++) {
+        for (std::size_t j = 0; j < J; j++) {
+            double v = 0.0;
+            if (operation == 1) {
+                v = (bool)mask(i, j) && (v1[i] == v2[j]);
+            } else if (operation == 2) {
+                v = (bool)mask(i, j) || (v1[i] == v2[j]);
+            } else {
+                v = (bool)mask(i, j) != (v1[i] == v2[j]);
+            }
+            simil_tri.push_back(std::make_tuple(i, j, v));
+        }
+    }
+# endif
+    return to_matrix(simil_tri, I, J, false, true);
+}
+
 /***R
 system.time(
     out <- cpp_mask(sample(1:100, 10000, replace = TRUE), sample(1:100, 10000, replace = TRUE))
